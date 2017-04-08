79d7edf28b19befcb026fee4fa6bafc98a0df984
Matias <mpierdominici@itba.edu.ar>
2017-04-08 20:02:22 -0300

    SE AGREGARON LAS DECLARACIOS DE GENERCI STATE

---
+++ TP5_EDA_FCM/TP5_EDA_FCM/genericState.cpp
@@ -0,0 +1,31 @@
+#include "genericState.h"
+
+genericState * genericState::onRWrq(genericEvent * ev)
+{
+	return NULL;
+}
+
+genericState * genericState::onRRrq(genericEvent * ev)
+{
+	return NULL;
+}
+
+genericState * genericState::onTimeOut(genericEvent * ev)
+{
+	return NULL;
+}
+
+genericState * genericState::onError(genericEvent * ev)
+{
+	return NULL;
+}
+
+genericState * genericState::onRack(genericEvent * ev)
+{
+	return NULL;
+}
+
+genericState * genericState::onRdata(genericEvent * ev)
+{
+	return NULL;
+}
