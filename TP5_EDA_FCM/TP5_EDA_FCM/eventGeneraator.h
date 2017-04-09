9094ea52f9e51e42a986d416c4fc670e736a2160
Matias <mpierdominici@itba.edu.ar>
2017-04-08 23:18:44 -0300

    se creo parte del event generator, falta los new de las clasesheredadas de evento

---
+++ TP5_EDA_FCM/TP5_EDA_FCM/eventGeneraator.h
@@ -0,0 +1,11 @@
+
+
+
+
+#include "genericEvent.h"
+
+genericEvent * eventGenerator();
+
+
+
+
---
+++ TP5_EDA_FCM/TP5_EDA_FCM/eventGenerator.cpp
@@ -0,0 +1,27 @@
+
+#include <Cstdio>
+#include "eventGeneraator.h"
+
+genericEvent * eventGenerator()
+{
+	switch (getchar())
+	{
+	case 'a':
+		//new de cada tipo de envento
+		break;
+	case 's':
+		break;
+	case 'd':
+		break;
+	case 'f':
+		break;
+	case 'g':
+		break;
+	case 'h':
+		break;
+	default:
+		return NULL;
+		break;
+	}
+
+}
